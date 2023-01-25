77b313d60ed96449b072d938a78433d2078d3509
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-25 10:14:26 -0600

    creates a greet response object

--- src/main/java/com/justin/Main.java
+++ src/main/java/com/justin/Main.java
@@ -2,10 +2,36 @@ package com.justin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/")
+    public GreetResponse greet() {
+        GreetResponse response = new GreetResponse(
+                "Hello",
+                List.of("Java", "Golang", "JavaScript"),
+                new Person("Alex")
+        );
+        return response;
+    }
+
+    record Person(String name) {
+
+    }
+
+    record GreetResponse(
+            String greet,
+            List<String> favoriteProgrammingLanguages,
+            Person person
+    ) {
     }
 }
