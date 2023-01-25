c718e7306ab35804d0cf9e21bf210b4059f209e1
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-25 17:30:26 -0600

    allows deleting customer

--- src/main/java/com/justin/Main.java
+++ src/main/java/com/justin/Main.java
@@ -2,10 +2,7 @@ package com.justin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -43,6 +40,11 @@ public class Main {
         customer.setEmail(request.email);
         customer.setAge(request.age);
         customerRepository.save(customer);
+    }
+
+    @DeleteMapping({"customerId"})
+    public void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
     }
 
 }
